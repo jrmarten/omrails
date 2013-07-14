f8b7b1e4fa51ae89464e6b8e1436022876addefa
Jameson Marten <jrmarten@gmail.com>
2013-07-14 10:58:15 -0400

    Added Bootsrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Omrails::Application.routes.draw do
   get 'about' => 'pages#about'
-  root 'pages#home'
+  root :to => 'pages#home'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
